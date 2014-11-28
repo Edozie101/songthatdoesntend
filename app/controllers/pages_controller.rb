dd128355cbce5566b1f8038c2ad3d41a6c587bfb
edzye101 <edzye101@gmail.com>
2014-11-27 17:42:21 -0800

    Added MP3 files and youtube page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,3 +1,15 @@
 class PagesController < ApplicationController
 
+  def versetoo
+
+  end
+  def versetree
+  end
+  def versefor
+  end
+  def youtube
+
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
   root to: "pages#index"
+
+  get 'verse-2' => "pages#versetoo", as: :versetoo
+
+  get 'verse-3'=> "pages#versetree"
+
+  get 'verse-4'=> "pages#versefor"
+
+  get 'root' => "pages#index"
+
+  get 'youtube' => "pages#youtube"
+
 end
